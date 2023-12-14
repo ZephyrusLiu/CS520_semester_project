1efad6f38b857833b5b463f5dde2fa41cc7153b6
nenevadera <yjinjin2001@gmail.com>
2023-12-13 21:16:31 -0500

    Change homepage related files

--- src/server.js
+++ src/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+app.use(fileUpload({
+	createParentPath: true
+}));
+
+// Check if the user is authenticated by the system.
+checkAuthenticated = (req, res, next) => {
+	if (req.isAuthenticated()) {
+		return next();
+	} else {
+		res.status(401).redirect('../login.html');
+	}
+};
+
 app.get('/', function (req, res) {
 	console.log(req.params);
-	res.sendFile(__dirname + '/home_page.html');
+	res.sendFile(__dirname + '/homepage.html');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
