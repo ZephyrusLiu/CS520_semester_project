20b79e34389389e2302bac3170540d2fec0805ca
nenevadera <yjinjin2001@gmail.com>
2023-12-13 20:58:32 -0500

    Modify server.js

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
 	console.log(req.params);
-	res.sendFile(__dirname + '/dashboard.html');
+	res.sendFile(__dirname + '/homepage.html');
 });
 
 app.listen(port, () => {
